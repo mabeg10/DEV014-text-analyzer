39ba04e3d1ee312426158849ae4679a1d3ef5013
mbguzman.099@gmail.com <mabe@192.168.1.9>
2024-03-14 12:03:48 -0500

    últimos cambios de js

--- src/index.js
+++ src/index.js
@@ -2,67 +2,70 @@ import analyzer from './analyzer.js';
 
 const inputText = document.querySelector('textarea[name="user-input"]');
 inputText.addEventListener('input', function() {
-    // Aquí se obtiene el texto ingresado por la usuaria
-    const texto = inputText.value;
-// Seleccionar todos los elementos <li> dentro de un <ul>
+  // Aquí se obtiene el texto ingresado por la usuaria
+  const texto = inputText.value;
+  // Seleccionar todos los elementos <li> dentro de un <ul>
   const elementosLi = document.querySelectorAll("ul > li");
 
-// Función 1 
-    const primerElementoLi = elementosLi[0]; 
+  // Función 1 
+  const primerElementoLi = elementosLi[0]; 
    
-    const wordcount = analyzer.getWordCount(texto);
+  const wordcount = analyzer.getWordCount(texto);
 
-    primerElementoLi.textContent = "Palabras: " + wordcount;
+  primerElementoLi.textContent = "Palabras: " + wordcount;
 
-// Función 2 
-   const segundoElementoLi = elementosLi[1];
+  // Función 2 
+  const segundoElementoLi = elementosLi[1];
 
-   const charactercount = analyzer.getCharacterCount(texto);
+  const charactercount = analyzer.getCharacterCount(texto);
 
-   segundoElementoLi.textContent = "Caracteres: " + charactercount;
+  segundoElementoLi.textContent = "Caracteres: " + charactercount;
     
-//  Función 3
-    const tercerElementoLi = elementosLi[2];
+  //  Función 3
+  const tercerElementoLi = elementosLi[2];
 
-    const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(texto);
+  const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(texto);
   
-    tercerElementoLi.textContent = "Caracteres sin espacios y signos de puntuación: " + characterCountExcludingSpaces;
+  tercerElementoLi.textContent = "Caracteres sin espacios y signos de puntuación: " + characterCountExcludingSpaces;
 
- // Función 4
+  // Función 4
 
-    const cuartoElementoLi = elementosLi[3];
+  const cuartoElementoLi = elementosLi[3];
 
-    const numbercount = analyzer.getNumberCount (texto);
+  const numbercount = analyzer.getNumberCount (texto);
 
-    cuartoElementoLi.textContent = "Números: " + numbercount;
+  cuartoElementoLi.textContent = "Números: " + numbercount;
 
-// función 5 
+  // función 5 
 
-    const quintoElementoLi = elementosLi[4]
-    const numbersum = analyzer.getNumberSum(texto);
-    quintoElementoLi.textContent = "Suma de Números: " + numbersum;
+  const quintoElementoLi = elementosLi[4]
+  const numbersum = analyzer.getNumberSum(texto);
+  quintoElementoLi.textContent = "Suma de Números: " + numbersum;
 
-    // Función 6
+  // Función 6
 
-    const sextoElementoLi = elementosLi [5]
-    const datatestid = analyzer.getAverageWordLength(texto);
-    // Aquí despues de Plabras: "<--- hay un espacio, eso se ve reflejado en el analizador de textos"
-    sextoElementoLi.textContent = "Promedio Longitud Palabras: " + datatestid ;
+  const sextoElementoLi = elementosLi [5]
+  const wordlengthaverage = analyzer.getAverageWordLength(texto);
+  // Aquí despues de Plabras: "<--- hay un espacio, eso se ve reflejado en el analizador de textos"
+  sextoElementoLi.textContent = "Promedio Longitud Palabras: " +wordlengthaverage;
 
-    // Botón Limpiar
+  // Botón Limpiar
 
-    function setupResetButton() {
-        // hay que llamar al textarea usando querySelector por su atributo name
-        const textarea = document.querySelector('textarea[name="user-input"]');
-        const LimpiarMetricas = document.getElementById('reset-button');
+  function limpiarMetricas() {
+ 
+    inputText.value = '';
     
-        LimpiarMetricas.addEventListener('click', function() {
-            textarea.value = '';
-        });
-    }
-    // Hay que  Llamar a la función para que se ejecute o se configure el event listener
-    setupResetButton();
+    primerElementoLi.textContent = 'Palabras'
+    segundoElementoLi.textContent = 'Caracteres'
+    tercerElementoLi.textContent = 'Caracteres sin espacios y signos de puntuación'
+    cuartoElementoLi.textContent = 'Números'
+    quintoElementoLi.textContent = 'Suma de Números'
+    sextoElementoLi.textContent = 'Promedio Longitus Palabras'
+  }
+  const resetButton = document.getElementById('reset-button');
     
+    
+  resetButton.addEventListener('click', limpiarMetricas);
     
 
 });  
